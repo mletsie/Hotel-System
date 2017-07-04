ce19f0a9feacb7cd16e7f28abe26d004d80a320b
Masupha Letsie <tanzupa77@outlook.com>
2017-07-04 21:44:08 +0200

    Registration functionality now working

--- WpfApp1/Business Logic/Register.cs
+++ WpfApp1/Business Logic/Register.cs
@@ -15,16 +15,22 @@ namespace WpfApp1.Business_Logic
     {
         private DBconnect db;
 
-        public int individualRegister(string name, string surname, string dob, string email, string number, string password)
+        public int individualRegister(string name, string surname, DateTime dob, string email, string number, string password)
         {
-            string command = "INSERT INTO individual (Name, Surname, DateOfBirth, Email, ContactNumber, Password) VALUES (" + 
-                name + "," + surname + "," + "1958/09/01" + "," + "tsepo@mail.com" + "," + number + "," + password + ");";
+            string command = "INSERT INTO individual (Name, Surname, DateOfBirth, Email, ContactNumber, Password) VALUES (" +
+                "@name, @surname, @dob, @email, @number, @password);";
 
             try
             {
                 db = new DBconnect();
                 db.openConnection();
                 MySqlCommand cmd = new MySqlCommand(command, db.connection);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@surname", surname);
+                cmd.Parameters.AddWithValue("@dob", dob);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@number", number);
+                cmd.Parameters.AddWithValue("@password", password);
                 cmd.ExecuteNonQuery();
                 db.closeConnection();
                 return 1;
